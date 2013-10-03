40fbe747d674b7311b8f3bef3ca7e7012ddfe9ce
Mike Purvis <mpurvis@clearpathrobotics.com>
2013-10-03 15:35:34 -0400

    Rebase from 'debian/hydro/nmea_comms'

--- src/rx.cpp
+++ src/rx.cpp
@@ -5,12 +5,15 @@
 
 #include <poll.h>
 #include <sstream>
+#include <sys/ioctl.h>
 #include <boost/thread.hpp>
 #include <boost/algorithm/string.hpp>
 
 #include "ros/ros.h"
 #include "nmea_msgs/Sentence.h"
 
+#define RX_INITIAL_LENGTH 40
+#define RX_SUCCESSIVE_LENGTH 8
 
 static void _handle_sentence(ros::Publisher& publisher, ros::Time& stamp, const char* sentence, const char* frame_id)
 {
@@ -26,7 +29,7 @@ static void _handle_sentence(ros::Publisher& publisher, ros::Time& stamp, const 
 
 static int threads_active = 1;
  
-static void _thread_func(ros::NodeHandle& n, int fd, std::string frame_id)
+static void _thread_func(ros::NodeHandle& n, int fd, std::string frame_id, uint32_t byte_time_ns=0)
 {
   ROS_DEBUG("New connection handler thread beginning.");
 
@@ -40,6 +43,7 @@ static void _thread_func(ros::NodeHandle& n, int fd, std::string frame_id)
   char* buffer_end = &buffer[sizeof(buffer)];
 
   while(threads_active) {
+    errno = 0;
     int retval = poll(pollfds, 1, 500);
     ROS_DEBUG("Poll retval=%d, errno=%d, revents=%d", retval, errno, pollfds[0].revents);
 
@@ -59,13 +63,39 @@ static void _thread_func(ros::NodeHandle& n, int fd, std::string frame_id)
       ROS_DEBUG("Exiting handler thread.");
       return;
     }
+    
+    // We can save some CPU by sleeping if the number waiting bytes is really small
+    if (byte_time_ns > 0) {
+      int waiting_bytes;
+      errno = ioctl(fd, FIONREAD, &waiting_bytes);
+      if (errno == 0) {
+        int wait_for = 0;
+        int buffer_plus_waiting = (buffer_write - buffer) + waiting_bytes;
+        if (buffer_plus_waiting < RX_INITIAL_LENGTH) {
+          wait_for = RX_INITIAL_LENGTH - buffer_plus_waiting;
+        } else if (waiting_bytes < RX_SUCCESSIVE_LENGTH) {
+          wait_for = RX_SUCCESSIVE_LENGTH - waiting_bytes;
+        }
+        if (wait_for > 0) {
+          struct timespec req = { 0, wait_for * byte_time_ns }, rem;
+          ROS_DEBUG_STREAM("Sleeping for " << wait_for << " bytes (" << byte_time_ns << " ns)");
+          nanosleep(&req, &rem);
+        }
+      }
+    }
 
     // Read in contents of buffer and null-terminate it.
     ros::Time now = ros::Time::now();
     errno = 0;
     retval = read(fd, buffer_write, buffer_end - buffer_write - 1);
     ROS_DEBUG("Read retval=%d, errno=%d", retval, errno);
+    ROS_DEBUG_COND(retval < 0, "Read error: %s", strerror(errno));
     if (retval > 0) {
+      if (strnlen(buffer_write, retval) != retval) {
+        ROS_WARN("Null byte received from serial port, flushing buffer.");
+        buffer_write = buffer;
+        continue;
+      }
       buffer_write += retval;
     } else if (retval == 0) {
       ROS_INFO("Device stream ended.");
@@ -77,8 +107,14 @@ static void _thread_func(ros::NodeHandle& n, int fd, std::string frame_id)
       ROS_DEBUG("Exiting handler thread.");
       return; 
     } else {
-      ROS_FATAL("Error reading from device. retval=%d, errno=%d, revents=%d", retval, errno, pollfds[0].revents);
-      ros::shutdown();
+      // retval < 0, indicating an error of some kind.
+      if (errno == EAGAIN) {
+        // Can't read from the device, try again.
+        continue;
+      } else {
+        ROS_FATAL("Error reading from device. retval=%d, errno=%d, revents=%d", retval, errno, pollfds[0].revents);
+        ros::shutdown();
+      }
     }
     ROS_DEBUG_STREAM("Buffer size after reading from fd: " << buffer_write - buffer);
     *buffer_write = '\0';
@@ -95,8 +131,12 @@ static void _thread_func(ros::NodeHandle& n, int fd, std::string frame_id)
     }
 
     int remainder = buffer_write - buffer_read;
+    if (remainder > 2000) {
+      ROS_WARN("Buffer size >2000 bytes, resetting buffer.");
+      remainder = 0;
+    }
     ROS_DEBUG_STREAM("Remainder in buffer is: " << remainder);
-    memcpy(buffer, buffer_read, remainder);
+    memmove(buffer, buffer_read, remainder);
     buffer_write = buffer + remainder;
   }
   close(fd);
@@ -138,8 +178,8 @@ void rx_stop_all()
   ROS_INFO_STREAM("Closed " << thread_close_i << " thread(s) cleanly.");
 }
 
-void rx_thread_start(ros::NodeHandle& n, int fd, std::string frame_id)
+void rx_thread_start(ros::NodeHandle& n, int fd, std::string frame_id, uint32_t byte_time_ns)
 {
-  rx_threads.push_back(new boost::thread(_thread_func, boost::ref(n), fd, frame_id));
+  rx_threads.push_back(new boost::thread(_thread_func, boost::ref(n), fd, frame_id, byte_time_ns));
 }
 
